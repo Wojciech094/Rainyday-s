082b9d17fa28c12cdd7ec0fc6d3498b095f21e44
Wojciech094 <WPLesniak@hotmail.com>
2024-11-11 10:49:45 +0100

    Add files via upload

---
+++ Rainyday's/myscripts.js
@@ -0,0 +1,13 @@
+// document.getElementById('burger').addEventListener('click', function () {
+// 	const navBar = document.getElementById('nav-bar');
+// 	const searchContainer = document.getElementById('search-container');
+// 	navBar.classList.toggle('active');
+// 	if (navBar.classList.contains('active')) {
+// 		searchContainer.style.display = 'none';
+// 	} else {
+// 		searchContainer.style.display = 'flex';
+// 	}
+// });
+//  document.getElementById('burger').addEventListener('click', function () {
+// 		document.getElementById('nav-bar').classList.toggle('active');
+//  });
